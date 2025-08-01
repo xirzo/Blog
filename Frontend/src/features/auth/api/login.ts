9b5e232292dc91350f94410a24f5cf82e0fade4d
xirzo <easttem@gmail.com>
2025-08-01 15:43:30 +0300

    fix: wrong frontend login endpoint

--- Frontend/src/features/auth/api/login.ts
+++ Frontend/src/features/auth/api/login.ts
@@ -1,6 +1,6 @@
 import { api } from "../../../shared/api/axios";
 
 export async function login({ email, password }: { email: string, password: string }) {
-  const response = await api.post("/login", { email, password });
+  const response = await api.post("/auth/login", { email, password });
   return response.data;
 }
