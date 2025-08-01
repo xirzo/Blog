4f36d10ea756cce3ee2ef8d79260ed475ad9dcb7
xirzo <easttem@gmail.com>
2025-08-01 19:19:32 +0300

    build: hotreload frontend

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+  export default defineConfig({
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true, 
+      },
+      host: '0.0.0.0',
+      port: 3000,
+      strictPort: true,
+      hmr: {
+        clientPort: 3000,
+        host: '0.0.0.0'
+      }
+    }
+  }); 
