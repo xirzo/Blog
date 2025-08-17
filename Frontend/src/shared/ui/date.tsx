a50c8fa0fa5844fdc63136ce197066c692161c69
xirzo <easttem@gmail.com>
2025-08-17 13:00:28 +0300

    feat: formatted date component

---
+++ Frontend/src/shared/ui/date.tsx
@@ -0,0 +1,13 @@
+﻿import React from "react";
+
+function FormattedDate({date}: { date: Date | string | number }) {
+    const d = new Date(date);
+
+    if (isNaN(d.getTime())) {
+        return <span>Invalid date</span>;
+    }
+
+    return <span>{d.toLocaleDateString(undefined, {day: "numeric", month: "long", year: "numeric"})}</span>;
+}
+
+export default FormattedDate;
