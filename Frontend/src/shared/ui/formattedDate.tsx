c135195aa904599122c9334e328ef5a7c670e9df
xirzo <easttem@gmail.com>
2025-08-17 13:43:39 +0300

    feat: formatted date props for span

---
+++ Frontend/src/shared/ui/formattedDate.tsx
@@ -0,0 +1,17 @@
+﻿import React from "react";
+
+interface IFormattedDateProps extends React.HTMLAttributes<HTMLSpanElement> {
+    date: Date | string | number;
+}
+
+function FormattedDate({date, ...props}: IFormattedDateProps) {
+    const d = new Date(date);
+
+    if (isNaN(d.getTime())) {
+        return <span>Invalid date</span>;
+    }
+
+    return <span {...props}>{d.toLocaleDateString(undefined, {day: "numeric", month: "long", year: "numeric"})}</span>;
+}
+
+export default FormattedDate;
