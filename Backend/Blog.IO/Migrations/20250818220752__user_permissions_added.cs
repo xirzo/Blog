9f4a8d96a98cecbf2f3efbfe741f4858c45fb5b6
xirzo <easttem@gmail.com>
2025-08-19 01:19:01 +0300

    migration: user permissions added

---
+++ Backend/Blog.IO/Migrations/20250818220752__user_permissions_added.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.IO.Migrations
+{
+    /// <inheritdoc />
+    public partial class _user_permissions_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Permissions",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Permissions",
+                table: "Users");
+        }
+    }
+}
