9328766288e12656950c222a4295de22c8def25d
xirzo <easttem@gmail.com>
2025-08-19 01:17:35 +0300

    feat: permissions added

---
+++ Backend/Blog.Core/Entities/Permissions.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog.Core.Entities;
+
+
+/* Test permissions, all of these
+ need to be registered inside Program.cs, 
+ then added to individual user inside db. 
+ In order to allow access for this role, add
+ attribute with policy inside the controller 
+*/
+public static class Permissions
+{
+    public const string Create = nameof(Create);
+    public const string Update = nameof(Update);
+    public const string Delete = nameof(Delete);
+}
