d56fa9fb64930ef43fc3ef4aa87d8788c2e931d4
xirzo <easttem@gmail.com>
2025-08-20 20:45:41 +0300

    refactor: vite config bloat removal

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(() => ({
     plugins: [react(), tailwindcss()],
     server: {
-        watch: {
-            usePolling: true,
-        },
-        allowedHosts: ['xirzo.ru'],
-        host: '0.0.0.0',
         port: 3000,
-        strictPort: true,
-        hmr: {
-            host: 'localhost',
-            port: 3000
-        }
+        host: true,
     }
-}); 
+}));
